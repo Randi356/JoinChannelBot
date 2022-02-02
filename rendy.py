0ae4246d63f1be473062c34c08f3e217a5df4ae1
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-02-02 14:46:21 +0700

    Update rendy.py

--- rendy.py
+++ rendy.py
@@ -1,32 +1,34 @@
 # made by rendy
+
 import os
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyBoardMarkup, InlineKeyBoardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = Client(
-  "test bot",
-  bot_token = os.enverion.get("BOT_TOKEN"),
-  api_id = int(os.enverion.get("API_ID")),
-  api_hash = os.enverion.get("API_HASH"),
-)                             
-                             
-@Bot.on_message(filters, private & private.all)
+
+Bot = Client(
+    "Join-Channel-Bot",
+    bot_token = os.environ.get("BOT_TOKEN"),
+    api_id = int(os.environ.get("API_ID")),
+    api_hash = os.environ.get("API_HASH")
+)
+
+
+@Bot.on_message(filters.private & filters.all)
 async def text(bot, update):
-   
-  text = "join channel && join groups"
-  reply_markup = InlineKeyBoardMarkup(
-      [
-        [InlineKeyBoardButton(text="join channel", url="https://t.me/RendyProjects)],
-        [InlineKeyBoardButton(text="join groups", url="https://t.me/HomeSupport)]
-      ]
-                              
-   )
-   
-   await update.reply_text(
-       text=text,
-       reply_markup=reply_markup,
-       disable_web_page_preivew=False
-       quote=True
-   )
+    
+    text = "join channel & join groups.\n\nMade by @CuteInspire"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text="join channel", url="https://t.me/RendyProjects")],
+            [InlineKeyboardButton(text="Search in another chat", url="https://t.me/HomeSupport")]
+        ]
+    )
+    
+    await update.reply_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        quote=True
+    )
          
 Bot.run()         
