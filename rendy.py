661b64f43d343854bf025b7c052c396574a916ab
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-02-02 13:12:29 +0700

    Update rendy.py

--- rendy.py
+++ rendy.py
@@ -1,15 +1,31 @@
+# made by rendy
 import os
-import telebot
+from pyrogram import Client, filters
+from pyrogram InlineKeyBoardMarkup, InlineKeyBoardButton
 
-# my telegram org by rendy 
-API_KEY = os.getenv('API_KEY')
-bot = telebot.TeleBot(API_KEY)
-
-# chatbot commad by rendy
-bot.message_handler(commads=['gay'])
-def gay(message):
-  bot.reply_to(message, "hallo gay")
-  
-# running
-bot.polling()
-  
+bot = Client(
+  "test bot"
+  bot_token = os.enverion.get("BOT_TOKEN"),
+  api_id = int(os.enverion.get("API_ID")),
+  api_hash = os.enverion.get("API_HASH),
+                             
+@Bot.on_message(filters, private & private.all)
+async def text(bot, update):
+   
+  text = "join channel && join groups"
+  reply_markup = InlineKeyBoardMarkup(
+      [
+        [InlineKeyBoardButton(text="join channel", url="https://t.me/RendyProjects)],
+        [InlineKeyBoardButton(text="join groups", url="https://t.me/HomeSupport)]
+      ]
+                              
+   )
+   
+   await update.reply_text(
+       text=text,
+       reply_markup=reply_markup,
+       disable_web_page_preivew=False
+       quote=True
+   )
+         
+Bot.run()         
